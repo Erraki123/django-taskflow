6b47a4d7b9724aa70bb1c92056cee33f1e458ecf
Erraki123 <raki39174@gmail.com>
2026-06-30 00:07:46 +0100

    add gunicorn to requirements

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,12 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-key-change-me')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
+CSRF_TRUSTED_ORIGINS = config(
+    'CSRF_TRUSTED_ORIGINS',
+    default='https://*.up.railway.app',
+    cast=Csv()
+)
+
 # ----------------------------------------------------------------------
 # Applications
 # ----------------------------------------------------------------------
